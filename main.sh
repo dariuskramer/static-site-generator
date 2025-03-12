9797c0dcb584c1d2e60e61f92f1a39ae57f71e81
Donovan <commit@dkrm.dev>
2025-03-12 16:13:26 +0100

    add script to copy static files into public/

--- main.sh
+++ main.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+STATIC_DIR='static'
+PUBLIC_DIR='public'
+
+rm --verbose --preserve-root --recursive "${PUBLIC_DIR}"
+mkdir --verbose "${PUBLIC_DIR}"
+cp --verbose --recursive "${STATIC_DIR}"/* "${PUBLIC_DIR}/"
+
 python src/main.py
